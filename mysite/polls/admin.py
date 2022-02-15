6c48c51cdd4708eb61236fc3e8dd2f54ebbf77b6
Krishna <krishnakumarrai1998@gmail.com>
2022-02-16 01:26:58 +0530

    added search box and filter side bar in admin page
    search box will be used to search a particular question_text and filter
    side bar is used to filter the list using various fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,8 @@ from .models import Question,Choice
 # class QuestionAdmin(admin.ModelAdmin):
 #     fields = ['pub_date', 'question_text']
 # class QuestionAdmin(admin.ModelAdmin):
+#     list_display = ('question_text', 'pub_date', 'was_published_recently')
+# class QuestionAdmin(admin.ModelAdmin):
 #     fieldsets = [
 #         (None,               {'fields': ['question_text']}),
 #         ('Date information', {'fields': ['pub_date']}),
@@ -20,7 +22,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 admin.site.register(Question,QuestionAdmin)
 # admin.site.register(Choice)
 
