0dab18a13f11beab4fcc79235b345eb5c9e55b63
Krishna <krishnakumarrai1998@gmail.com>
2022-02-16 00:40:14 +0530

    created another way of adding choices for question
    previous method was inefficient way of adding choice objects to the
    system It’d be better if we can add a bunch of Choices directly when you create the Question object.
    ChoiceInline class will by default generate extraa choice objects and
    it work like this There are three slots for related Choices – as specified by extra – and each time
    you come back to the “Change” page for an already-created object, you get another three extra slots.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,8 +9,19 @@ from .models import Question,Choice
 #         (None,               {'fields': ['question_text']}),
 #         ('Date information', {'fields': ['pub_date']}),
 #     ]
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
+# admin.site.register(Choice)
 
 # Register your models here.
